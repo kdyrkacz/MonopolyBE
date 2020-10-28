4a3e1592143659addf9b9fa81186279e1988762b
Karol Dyrkacz <karoldyrkacz@discover.com>
2020-10-27 19:00:20 -0500

    Added initializer

---
+++ src/main/java/backend/game/monopoly/monopolyBE/MonopolyBeApplication.java
@@ -0,0 +1,13 @@
+package backend.game.monopoly.monopolyBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonopolyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonopolyBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/game/monopoly/monopolyBE/MonopolyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.game.monopoly.monopolyBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonopolyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
